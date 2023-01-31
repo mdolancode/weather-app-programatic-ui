c6e6b17f00b2f1d08eccef21c579981b0dbcb97e
Matthew Dolan <mdolancode@gmail.com>
2023-01-30 17:07:09 -0800

    Add style and layout for backgroundView.

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    
+    let backgroundView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
+}
 
-
+extension WeatherViewController {
+    
+    // Step 1: style
+    func style() {
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundView.image = UIImage(named: "day-background")
+        backgroundView.contentMode = .scaleAspectFill
+        
+    }
+    
+    
+    // Step 2: Auto layout
+    func layout() {
+        view.addSubview(backgroundView)
+        
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+    
 }
 
