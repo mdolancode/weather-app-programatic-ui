23cfb675d25a1d34f7a9c7d1dfe3048654c1e3a0
Matthew Dolan <mdolancode@gmail.com>
2023-01-31 12:40:15 -0800

    Add location button, search button and search text field.

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class WeatherViewController: UIViewController {
     
     let backgroundView = UIImageView()
+    let locationButton = UIButton()
+    let searchButton = UIButton()
+    let searchTextField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,22 +25,63 @@ extension WeatherViewController {
     
     // Step 1: style
     func style() {
+        // backgroundView
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
-        backgroundView.image = UIImage(named: "day-background")
+        backgroundView.image = UIImage(named: "background")
         backgroundView.contentMode = .scaleAspectFill
         
+        // location button
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
+        locationButton.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
+        locationButton.tintColor = .label
+        
+        // search button
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.tintColor = .label
+        
+        // search text field
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.font = UIFont.preferredFont(forTextStyle: .title1)
+        searchTextField.placeholder = "Search"
+        searchTextField.textAlignment = .right
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.backgroundColor = .systemFill
     }
     
     
     // Step 2: Auto layout
     func layout() {
+        // backgroundView
         view.addSubview(backgroundView)
+        // location button
+        view.addSubview(locationButton)
+        // search button
+        view.addSubview(searchButton)
+        // search text field
+        view.addSubview(searchTextField)
+        
         
         NSLayoutConstraint.activate([
+            // backgroundView
             backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            // location button
+            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            locationButton.widthAnchor.constraint(equalToConstant: 40),
+            locationButton.heightAnchor.constraint(equalToConstant: 40),
+            // search button
+            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
+            searchButton.widthAnchor.constraint(equalToConstant: 40),
+            searchButton.heightAnchor.constraint(equalToConstant: 40),
+            // search text field
+            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
+            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1)
         ])
         
     }
